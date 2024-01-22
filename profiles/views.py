a17e44161982de3feb7d32e7347b8a08d2984b4e
Javi FS <javier.fersal@gmail.com>
2024-01-22 12:07:53 +0000

    feat: Add login/logout and profile detail view

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,21 @@
 from rest_framework import generics
 from .models import Profile
 from .serializers import ProfileSerializer
+from hoodsap_api.permissions import IsOwnerOrReadOnly
 
 
 class ProfileList(generics.ListAPIView):
+    """
+    List all profiles
+    """
     serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
+
+
+class ProfileDetailView(generics.RetrieveUpdateAPIView):
+    """
+    Retrieve or update a profile if you are the owner
+    """
+    serializer_class = ProfileSerializer
+    permission_classes = [IsOwnerOrReadOnly]
     queryset = Profile.objects.all()
