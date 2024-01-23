6d04a78e184dce0551f5d95f26db5fe38cc9df99
Javi FS <javier.fersal@gmail.com>
2024-01-23 20:05:41 +0000

    feat: Add likes list and detail views and urls

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Like
+from .serializers import LikeSerializer
+from hoodsap_api.permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+
+class LikeList(generics.ListCreateAPIView):
+    """
+    Lists all likes, creates a new like if the user
+    is logged in
+    """
+    serializer_class = LikeSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+    queryset = Like.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieves a like or delete if it's the owner's
+    """
+    serializer_class = LikeSerializer
+    permission_classes = [
+        IsOwnerOrReadOnly
+    ]
+    queryset = Like.objects.all()
