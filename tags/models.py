fcaad4c5f53dd079bbf265cb56e9b48ba48db354
Javi FS <javier.fersal@gmail.com>
2024-01-24 13:47:17 +0000

    feat: Create tags app, model and serializer

---
+++ tags/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Tag(models.Model):
+    """
+    Tags model
+    """
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.name
