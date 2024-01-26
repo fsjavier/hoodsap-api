6bc7cd7114fe904596b2f8c159b2e14a3504e411
Javi FS <javier.fersal@gmail.com>
2024-01-26 08:32:56 +0000

    feat: Add user friendly time format to comments

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 from .models import PostComment, SocialEventComment
 
@@ -10,16 +11,25 @@ class PostCommentSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    created_at_naturaltime= serializers.SerializerMethodField()
+    updated_at_naturaltime = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_created_at_naturaltime(self, obj):
+        return naturaltime(obj.updated_at)
+
+    def get_updated_at_naturaltime(self, obj):
+        return naturaltime(obj.created_at)
+
     class Meta:
         model = PostComment
         fields = [
             'id', 'owner', 'content', 'post', 'created_at', 'updated_at',
-            'is_owner', 'profile_id', 'profile_image'
+            'is_owner', 'profile_id', 'profile_image', 'created_at_naturaltime',
+            'updated_at_naturaltime'
         ]
 
 
