6473922e58a600eb012c9cf5446c70defcf7323a
Javi FS <javier.fersal@gmail.com>
2024-02-12 10:46:43 +0000

    feat: Move image field to separate component

---
+++ frontend/src/components/FormImageField.js
@@ -0,0 +1,52 @@
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Asset from "../components/Asset";
+import Image from "react-bootstrap/Image";
+import styles from "../styles/PostCreateForm.module.css";
+import ButtonStyles from "../styles/CustomButton.module.css";
+
+const FormImageField = ({ image, imageInputRef, handleChangeImage }) => {
+  const uploadImageSrc =
+    "https://res.cloudinary.com/drffvkjy6/image/upload/v1707474874/upload_icon_bdzvjh.webp";
+
+  return (
+    <Form.Group>
+      {image ? (
+        <>
+          <figure className={styles.Image__Container}>
+            <Image src={image} className={styles.Image} />
+          </figure>
+          <div>
+            <Form.Label
+              htmlFor="image-upload"
+              className={`${ButtonStyles.Button} ${ButtonStyles.Primary}`}
+            >
+              Change the image
+            </Form.Label>
+          </div>
+        </>
+      ) : (
+        <Form.Label
+          htmlFor="image-upload"
+          className="d-flex justify-content-center"
+        >
+          <Asset
+            src={uploadImageSrc}
+            message="Click or tap to upload an image"
+            height={100}
+            width={100}
+          />
+        </Form.Label>
+      )}
+      <Form.File
+        ref={imageInputRef}
+        className="d-none"
+        id="image-upload"
+        accept="image/*"
+        onChange={handleChangeImage}
+      />
+    </Form.Group>
+  );
+};
+
+export default FormImageField;
