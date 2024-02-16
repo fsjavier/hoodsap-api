372df255dfde14337818974dd7f5a19162c9f48a
Javi FS <javier.fersal@gmail.com>
2024-02-16 14:06:00 +0000

    fix: typo in detail view url

--- locations/urls.py
+++ locations/urls.py
@@ -4,5 +4,5 @@ from locations import views
 
 urlpatterns = [
     path('locations/', views.LocationList.as_view()),
-    path('locations/<int:pk>', views.LocationList.as_view()),
+    path('locations/<int:pk>', views.LocationDetail.as_view()),
 ]
