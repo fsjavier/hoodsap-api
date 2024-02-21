f134521974cb9e277ccae00ee9f2f7c61d67beef
Javi FS <javier.fersal@gmail.com>
2024-02-21 14:12:34 +0000

    fix: typo in serializer profile image

--- comments/serializers.py
+++ comments/serializers.py
@@ -10,7 +10,7 @@ class PostCommentSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
-    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.avatar.url')
     created_at_naturaltime= serializers.SerializerMethodField()
     updated_at_naturaltime = serializers.SerializerMethodField()
 
