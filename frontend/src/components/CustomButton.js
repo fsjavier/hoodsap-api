a2dee643f57b1e7e68ea7f2cf7286a4edb33cd18
Javi FS <javier.fersal@gmail.com>
2024-03-04 17:28:11 +0000

    feat: Add styles to CustomButton

--- frontend/src/components/CustomButton.js
+++ frontend/src/components/CustomButton.js
@@ -1,14 +1,27 @@
 import React from "react";
 import styles from "../styles/CustomButton.module.css";
 
-function CustomButton({ children, type, variant, disabled, onClick }) {
-  const variantClassName = styles[variant] || styles.Primary
+function CustomButton({
+  children,
+  type = "button",
+  variant = "Primary",
+  size = "Medium",
+  block = false,
+  disabled = false,
+  selected = false,
+  onClick,
+}) {
+  const variantClassName = styles[variant] || styles.Primary;
+  const sizeClassName = styles[size] || "";
+  const blockClassName = block ? styles.Block : "";
+  const selectedClassName = selected ? styles.Selected : "";
+
   return (
     <button
       type={type}
       onClick={onClick}
       disabled={disabled}
-      className={`${styles.Button} ${variantClassName}`}
+      className={`${styles.Button} ${variantClassName} ${sizeClassName} ${blockClassName} ${selectedClassName}`}
     >
       {children}
     </button>
