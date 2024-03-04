997b4ec63edd72d9ef0561cbb3f1611b64ac977a
Javi FS <javier.fersal@gmail.com>
2024-03-04 00:07:07 +0000

    feat: Add lat, lng, radius filter

--- hoodsap_api/serializers.py
+++ hoodsap_api/serializers.py
@@ -1,12 +1,18 @@
 from rest_framework import serializers
 from dj_rest_auth.serializers import UserDetailsSerializer
+from profiles.serializers import ProfileSerializer
 
 
 class UserSerializer(UserDetailsSerializer):
     profile_id = serializers.ReadOnlyField(source='profile.id')
-    profile_image = serializers.ReadOnlyField(source="profile.avatar.url")
+    profile_image = serializers.ReadOnlyField(source='profile.avatar.url')
+    profile_location_data = serializers.SerializerMethodField()
 
     class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + (
-            'profile_id', 'profile_image'
+            'profile_id', 'profile_image', 'profile_location_data'
         )
+
+    def get_profile_location_data(self, obj):
+        profile = obj.profile
+        return ProfileSerializer(profile, context=self.context).data.get('location_data')
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,4 @@
+from geopy.distance import geodesic as GD
 from django.db.models import Count
 from rest_framework import generics, permissions, filters
 from django_filters.rest_framework import DjangoFilterBackend
@@ -10,10 +11,6 @@ class PostList(generics.ListCreateAPIView):
     permission_classes = [
         permissions.IsAuthenticatedOrReadOnly
     ]
-    queryset = Post.objects.annotate(
-        comments_count=Count('postcomment', distinct=True),
-        likes_count=Count('likes', distinct=True)
-    ).order_by('-created_at')
     serializer_class = PostSerializer
     filter_backends = [
         filters.OrderingFilter,
@@ -37,6 +34,32 @@ class PostList(generics.ListCreateAPIView):
         # user posts
         'owner__profile'
     ]
+
+    def get_queryset(self):
+        queryset = Post.objects.annotate(
+            comments_count=Count('postcomment', distinct=True),
+            likes_count=Count('likes', distinct=True)
+        ).order_by('-created_at')
+        
+        latitude = self.request.query_params.get('latitude', None)
+        longitude = self.request.query_params.get('longitude', None)
+        radius = self.request.query_params.get('radius', None)
+
+        if latitude is not None and longitude is not None and radius is not None:
+            radius = float(radius)
+            latitude = float(latitude)
+            radius = float(radius)
+            user_location = (latitude, longitude)
+
+            filtered_posts = []
+            for post in queryset:
+                if post.location:
+                    post_location = (post.location.latitude, post.location.longitude)
+                    distance = GD(user_location, post_location).meters
+                    if distance <= radius:
+                        filtered_posts.append(post.id)
+            queryset = queryset.filter(id__in=filtered_posts)
+        return queryset
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
