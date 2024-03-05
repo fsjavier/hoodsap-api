b8627b26daafb875bebbfbab12985f3961c68e6f
Javi FS <javier.fersal@gmail.com>
2024-03-05 11:40:01 +0000

    feat: Filter events by location

--- social_events/views.py
+++ social_events/views.py
@@ -1,3 +1,4 @@
+from geopy.distance import geodesic as GD
 from django.db.models import Count
 from rest_framework import generics, permissions, filters
 from django_filters.rest_framework import DjangoFilterBackend
@@ -15,9 +16,9 @@ class SocialEventList(generics.ListCreateAPIView):
     permission_classes = [
         permissions.IsAuthenticatedOrReadOnly
     ]
-    queryset = SocialEvent.objects.annotate(
-       comments_count=Count('socialeventcomment', distinct=True) 
-    )
+    # queryset = SocialEvent.objects.annotate(
+    #    comments_count=Count('socialeventcomment', distinct=True) 
+    # )
 
     filter_backends = [
         filters.OrderingFilter,
@@ -33,6 +34,31 @@ class SocialEventList(generics.ListCreateAPIView):
         'indoor_outdoor',
         'owner__profile'
     ]
+
+    def get_queryset(self):
+        queryset = SocialEvent.objects.annotate(
+            comments_count=Count('socialeventcomment', distinct=True)
+        ).order_by('-created_at')
+        
+        latitude = self.request.query_params.get('latitude', None)
+        longitude = self.request.query_params.get('longitude', None)
+        radius = self.request.query_params.get('radius', None)
+
+        if latitude is not None and longitude is not None and radius is not None:
+            radius = float(radius)
+            latitude = float(latitude)
+            radius = float(radius)
+            user_location = (latitude, longitude)
+
+            filtered_events = []
+            for event in queryset:
+                if event.location:
+                    event_location = (event.location.latitude, event.location.longitude)
+                    distance = GD(user_location, event_location).meters
+                    if distance <= radius:
+                        filtered_events.append(event.id)
+            queryset = queryset.filter(id__in=filtered_events)
+        return queryset
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
